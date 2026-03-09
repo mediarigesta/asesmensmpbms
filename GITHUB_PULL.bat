69fcfc217d50b8b9e387c0ab145ed789a42e37d0
mediarigesta <anselmusgestawan@gmail.com>
2026-03-09 20:21:10 +0700

    commit

--- GITHUB_PULL.bat
+++ GITHUB_PULL.bat
@@ -3,35 +3,23 @@ echo ================================
 echo   Budi Mulia Exam - GitHub Pull
 echo ================================
 echo.
-
 git config --global user.name "mediarigesta"
 git config --global user.email "anselmusgestawan@gmail.com"
-
+git branch --set-upstream-to=origin/master master >nul 2>&1
 echo [1/3] Menyimpan perubahan lokal sementara...
 git stash
-if %errorlevel% neq 0 (
-    echo INFO: Tidak ada perubahan lokal untuk disimpan.
-)
-
 echo.
 echo [2/3] Mengambil update terbaru dari GitHub...
-git pull
+git pull origin master --allow-unrelated-histories
 if %errorlevel% neq 0 (
-    echo.
-    echo ERROR: Pull gagal! Cek koneksi internet.
-    echo Mengembalikan perubahan lokal...
-    git stash pop
+    echo ERROR: Pull gagal!
+    git stash pop 2>nul
     pause
     exit /b 1
 )
-
 echo.
 echo [3/3] Mengembalikan perubahan lokal...
-git stash pop
-if %errorlevel% neq 0 (
-    echo INFO: Tidak ada perubahan lokal untuk dikembalikan.
-)
-
+git stash pop 2>nul
 echo.
 echo ================================
 echo   Berhasil update dari GitHub!
